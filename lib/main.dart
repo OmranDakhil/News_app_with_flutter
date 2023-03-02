3e534fa03b646310edf62e1facef01078a055f96
omran dakhil <omranda1997@gmail.com>
2023-03-02 09:50:58 +0300

    init hive and open articles box

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:untitled11/views/home_page.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox("articles box");
   runApp(const MyApp());
 }
 
